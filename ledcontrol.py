a6847b9289a481668407c5f2c8a8c889347f7504
jeff07050055 <stupid5701@gmail.com>
2020-12-01 23:14:54 +0800

    modified:   ledcontrol.py

--- ledcontrol.py
+++ ledcontrol.py
@@ -12,26 +12,25 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(24,GPIO.OUT)
 
-deviceId = "DIfiiQka"
-deviceKey = "UNfsEdWeHFYYTGTt"
+deviceId = "DEURqOut"
+deviceKey = "5so6ld7ois0SBmii"
 
-# Set MediaTek Cloud Sandbox (MCS) Connection
 def get_to_mcs():
 	host = "http://api.mediatek.com"
-	endpoint = "/mcs/v2/devices/" + deviceId + "/datachannels/LEDControl/datapoints"
+	endpoint = "/mcs/v2/devices/" + deviceId + "/datachannels/LEDcontrol/datapoints"
 	url = host + endpoint
 	headers = {"Content-type": "application/json", "deviceKey": deviceKey}
 	r = requests.get(url,headers = headers)
 	value = (r.json()["dataChannels"][0]["dataPoints"][0]["values"]["value"])
 	return value
 
-
-while True:
+# Receive the Data from MCS
+while(True):
 	if(get_to_mcs() == 1):
-		print("LED on.")
+		print("LED turning on.")
 		GPIO.output(24,GPIO.HIGH)
 		time.sleep(0.5)
 	elif(get_to_mcs() == 0):
-		print("LED off.")
+		print("LED turning off.")
 		GPIO.output(24,GPIO.LOW)
 		time.sleep(0.5)
